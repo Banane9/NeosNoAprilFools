ca22870a688c260723853df14dd34b0e987e13ff
Arne Kiesewetter <banane9@live.de>
2023-04-01 13:56:07 +0200

    Add project files.

---
+++ NoAprilFools/NoAprilFools.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using BaseX;
+using CodeX;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+using FrooxEngine.LogiX.Data;
+using FrooxEngine.LogiX.ProgramFlow;
+using FrooxEngine.UIX;
+using HarmonyLib;
+using NeosModLoader;
+
+namespace NoAprilFools
+{
+    public class NoAprilFools : NeosMod
+    {
+        public override string Author => "Banane9";
+        public override string Link => "https://github.com/Banane9/NeosNoAprilFools";
+        public override string Name => "NoAprilFools";
+        public override string Version => "1.0.0";
+
+        public override void OnEngineInit()
+        {
+            var harmony = new Harmony($"{Author}.{Name}");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Engine))]
+        private static class EnginePatch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch(nameof(Engine.IsAprilFools))]
+            private static bool IsAprilFoolsPrefix(ref bool __result)
+            {
+                __result = false;
+                return false;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+Dynamic Variable Space Tree
+===========================
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that disables April Fools easter eggs for people who don't like fun.
+
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [NoAprilFools.dll](https://github.com/Banane9/NeosNoAprilFools/releases/latest/download/NoAprilFools.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
